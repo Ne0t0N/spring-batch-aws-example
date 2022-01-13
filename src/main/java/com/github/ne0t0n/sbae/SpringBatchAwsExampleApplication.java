39d3812ea1fc5a7402f2be20fbc530d770f0b95c
Oleksandr Shmatko <oshmatko@ebay.com>
2022-01-13 21:32:47 +0100

    Initial commit (base project structure)

---
+++ src/main/java/com/github/ne0t0n/sbae/SpringBatchAwsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.ne0t0n.sbae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAwsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchAwsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ne0t0n/sbae/SpringBatchAwsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ne0t0n.sbae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchAwsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
